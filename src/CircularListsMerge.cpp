4530daa59f6f4d4a0c9c82b1e9d60632aa4465e8
dasamantaraoaditya <dasamantaraoaditya@missionrnd.com>
2016-04-02 21:23:25 -0700

    Submit Commit

--- src/CircularListsMerge.cpp
+++ src/CircularListsMerge.cpp
@@ -20,7 +20,7 @@ pointing towards the first node.
 Note : Return -1 for Invalid Inputs like NULL;
 
 Input : head1 and head2 are Addresses of Two Circular Linked Lists heads .
-Output :Return Length of New SLL .Modify the head1 value, such that it now points to 
+Output :Return Length of New SLL .Modify the head1 value, such that it now points to
 Merged Sorted Circular SLL .
 
 Difficulty : Medium
@@ -38,9 +38,9 @@ int merge_circularlists(struct node **head1, struct node **head2){
 	if (*head1 == NULL&&*head2 == NULL)
 	{
 		head1 = NULL;
-		return -1;
+		return len;
 	}
-	struct node * start1=*head1,*start2=*head2,*final=NULL,*final_t=NULL;
+	struct node * start1 = *head1, *start2 = *head2, *final = NULL, *final_t = NULL;
 	if (start1->data > start2->data)
 	{
 		final_t = start2;
@@ -56,12 +56,12 @@ int merge_circularlists(struct node **head1, struct node **head2){
 	{
 		if (start1->data > start2->data)
 		{
-			final ->next= start2;
+			final->next = start2;
 			start2 = start2->next;
 		}
 		else
 		{
-			final ->next= start1;
+			final->next = start1;
 			start1 = start1->next;
 		}
 		final = final->next;
@@ -77,12 +77,12 @@ int merge_circularlists(struct node **head1, struct node **head2){
 
 	while (start2->next != *head2)
 	{
-		final ->next= start2;
+		final->next = start2;
 		final = final->next;
 		start2 = start2->next;
 		len++;
 	}
 	final->next = final_t;
 	*head1 = final_t;
-	return len;
+	return len + 2;
 }
